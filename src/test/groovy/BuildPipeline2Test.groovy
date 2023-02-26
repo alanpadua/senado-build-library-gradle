c6bf72d8d84481558c2d39db93acccde7c17586e
Alan <alan.padua@senado.leg.br>
2023-02-26 16:46:13 -0300

    teste

--- src/test/groovy/BuildPipeline2Test.groovy
+++ src/test/groovy/BuildPipeline2Test.groovy
@@ -10,50 +10,50 @@ import static com.lesfurets.jenkins.unit.global.lib.LocalSource.localSource
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class BuildPipeline2Test extends BasePipelineTest {
 
-//    String sharedLibs = new File( System.getProperty("user.dir")+ '//build//libs//senado-build-library-gradle.jar' )
-//    String sharedLibs = new File( System.getProperty("user.dir")+ '//build//libs//' )
-    String sharedLibs = new File( System.getProperty("user.dir") )
-
-    @Override
-    @BeforeAll
-    void setUp() throws Exception {
-
-        scriptRoots += 'vars'
-//        dirPath += '/senado-build-library-gradle'
-        helper.registerAllowedMethod('pipeline', [Closure.class], null)
-//        helper.registerAllowedMethod('options', [Closure.class], null)
-//        helper.registerAllowedMethod('timeout', [Map.class], null)
-//        helper.registerAllowedMethod('timestamps', [], null)
-//        helper.registerAllowedMethod('agent', [Closure.class], null)
-//        helper.registerAllowedMethod('stages', [Closure.class], null)
-//        helper.registerAllowedMethod('steps', [Closure.class], null)
-//        helper.registerAllowedMethod('script', [Closure.class], null)
-//        helper.registerAllowedMethod('readMavenPom', [Map.class], null)
-//        binding.setVariable('none', {})
-//        binding.setVariable('any', {})
-//        String clonePath = '/tmp'
+////    String sharedLibs = new File( System.getProperty("user.dir")+ '//build//libs//senado-build-library-gradle.jar' )
+////    String sharedLibs = new File( System.getProperty("user.dir")+ '//build//libs//' )
+//    String sharedLibs = new File( System.getProperty("user.dir") )
 //
-//        println('localSource(dirPath):' + localSource(sharedLibs))
-//        println('sharedLibs:' + sharedLibs)
+//    @Override
+//    @BeforeAll
+//    void setUp() throws Exception {
 //
-//        def library = library()
-//                .name('senado-build-library-gradle')
-//                .allowOverride(true)
-//                .implicit(false)
-//                .targetPath(sharedLibs)
-//                .defaultVersion("master")
-//                .retriever(localSource(sharedLibs))
-//                .build()
+//        scriptRoots += 'vars'
+////        dirPath += '/senado-build-library-gradle'
+//        helper.registerAllowedMethod('pipeline', [Closure.class], null)
+////        helper.registerAllowedMethod('options', [Closure.class], null)
+////        helper.registerAllowedMethod('timeout', [Map.class], null)
+////        helper.registerAllowedMethod('timestamps', [], null)
+////        helper.registerAllowedMethod('agent', [Closure.class], null)
+////        helper.registerAllowedMethod('stages', [Closure.class], null)
+////        helper.registerAllowedMethod('steps', [Closure.class], null)
+////        helper.registerAllowedMethod('script', [Closure.class], null)
+////        helper.registerAllowedMethod('readMavenPom', [Map.class], null)
+////        binding.setVariable('none', {})
+////        binding.setVariable('any', {})
+////        String clonePath = '/tmp'
+////
+////        println('localSource(dirPath):' + localSource(sharedLibs))
+////        println('sharedLibs:' + sharedLibs)
+////
+////        def library = library()
+////                .name('senado-build-library-gradle')
+////                .allowOverride(true)
+////                .implicit(false)
+////                .targetPath(sharedLibs)
+////                .defaultVersion("master")
+////                .retriever(localSource(sharedLibs))
+////                .build()
+////
+////        helper.registerSharedLibrary(library)
+//        super.setUp()
+//    }
 //
-//        helper.registerSharedLibrary(library)
-        super.setUp()
-    }
-
-    @Test
-    void test() {
-        runScript("job/library/Jenkinsfile")
-        printCallStack()
-    }
+//    @Test
+//    void test() {
+//        runScript("job/library/Jenkinsfile")
+//        printCallStack()
+//    }
 
 }
 
--- src/test/groovy/BuildPipelineTest.groovy
+++ src/test/groovy/BuildPipelineTest.groovy
@@ -1,58 +1,73 @@
 import com.lesfurets.jenkins.unit.BasePipelineTest
+import com.lesfurets.jenkins.unit.PipelineTestHelper
+import org.junit.Before
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 
 import static com.lesfurets.jenkins.unit.global.lib.GitSource.gitSource
 import static com.lesfurets.jenkins.unit.global.lib.LibraryConfiguration.library
+import static com.lesfurets.jenkins.unit.global.lib.LocalSource.localSource
 
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class BuildPipelineTest extends BasePipelineTest {
 
+    @Delegate PipelineTestHelper pipelineTestHelper
+//
+//    @Override
+//    @BeforeAll
+//    void setUp() throws Exception {
+////        scriptRoots += 'vars'
+//        super.setUp()
+////        helper.registerAllowedMethod('pipeline', [Closure.class], null)
+////        helper.registerAllowedMethod('options', [Closure.class], null)
+////        helper.registerAllowedMethod('timeout', [Map.class], null)
+////        helper.registerAllowedMethod('timestamps', [], null)
+////        helper.registerAllowedMethod('agent', [Closure.class], null)
+////        helper.registerAllowedMethod('stages', [Closure.class], null)
+////        helper.registerAllowedMethod('steps', [Closure.class], null)
+////        helper.registerAllowedMethod('script', [Closure.class], null)
+////        helper.registerAllowedMethod('readMavenPom', [Map.class], null)
+////        binding.setVariable('none', {})
+////        binding.setVariable('any', {})
+////        String clonePath = '/tmp'
+////
+////        def library = library()
+////                .name('senado-build-library-gradle')
+////                .retriever(gitSource('git@github.com:alanpadua/senado-build-library-gradle.git'))
+////                .targetPath(clonePath)
+////                .defaultVersion("main")
+////                .allowOverride(true)
+////                .implicit(false)
+////                .build()
+////
+////        helper.registerSharedLibrary(library)
+//    }
     @Override
     @BeforeAll
     void setUp() throws Exception {
-        scriptRoots += 'vars'
-        super.setUp()
-        helper.registerAllowedMethod('pipeline', [Closure.class], null)
-        helper.registerAllowedMethod('options', [Closure.class], null)
-        helper.registerAllowedMethod('timeout', [Map.class], null)
-        helper.registerAllowedMethod('timestamps', [], null)
-        helper.registerAllowedMethod('agent', [Closure.class], null)
-        helper.registerAllowedMethod('stages', [Closure.class], null)
-        helper.registerAllowedMethod('steps', [Closure.class], null)
-        helper.registerAllowedMethod('script', [Closure.class], null)
-        helper.registerAllowedMethod('readMavenPom', [Map.class], null)
-        binding.setVariable('none', {})
-        binding.setVariable('any', {})
-        String clonePath = '/tmp'
+        super.setUp();
+
+        helper.registerAllowedMethod('assertEquals', [Map.class], { 'ok' });
+        helper.registerAllowedMethod('assertEquals', [String, String], { "ok" });
 
         def library = library()
                 .name('senado-build-library-gradle')
                 .retriever(gitSource('git@github.com:alanpadua/senado-build-library-gradle.git'))
-                .targetPath(clonePath)
+                .targetPath('./')
                 .defaultVersion("main")
                 .allowOverride(true)
                 .implicit(false)
                 .build()
-
         helper.registerSharedLibrary(library)
     }
 
     @Test
     void test() {
-//        String clonePath = '/tmp'
-//        def library = library()
-//                .name('senado-build-library-gradle')
-//                .retriever(gitSource('git@github.com:alanpadua/senado-build-library-gradle.git'))
-//                .targetPath(clonePath)
-//                .defaultVersion("main")
-//                .allowOverride(true)
-//                .implicit(false)
-//                .build()
-//        helper.registerSharedLibrary(library)
-        runScript("job/library/Jenkinsfile")
+        def script = loadScript('src/test/groovy/Jenkinsfile')
+        script.run()
+//        runScript("job/library/Jenkinsfile")
         printCallStack()
     }
 }
